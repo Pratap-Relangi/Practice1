852aa23349f3ae4326ca94ce594e78e246cf7da4
Relangi Bala Surya Pratap <108869572+Pratap-Relangi@users.noreply.github.com>
2022-12-14 11:15:23 +0530

    Update Jenkins_Practice.py

--- Jenkins_Practice.py
+++ Jenkins_Practice.py
@@ -1,7 +1,8 @@
 list1=[10,20,30,40,50]
 res_list1=[x*2 for in list1]
-print(res_list)
+print(res_list1)
 list_2=[100,200,300,400,500]
 res_list2=[x*0.2 for in list_2]
 print(list_2)
+print(res_list2)
 print("list completed")
